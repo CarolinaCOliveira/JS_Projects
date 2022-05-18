16e0b97276185baa7f722eb8aab75e4b450ce359
Carolina Oliveira <carolina.oliveiraa@outlook.com>
2022-05-17 22:31:23 -0300

    Repositório JS

---
+++ Desafios DIO/JavaScript/coxinha_bueno.js
@@ -0,0 +1,7 @@
+
+
+let line = gets().split(" ");
+let A = parseInt(line[0]);
+let B = parseInt(line[1]);
+let total = A / B;     
+print(total.toFixed(2));
---
+++ Projetos/Interface_Netflix/README.md
@@ -0,0 +1,2 @@
+# Interface_Netflix
+Recriando a Interface da Netflix - Bootcamp WEB Developer Carrefour - DIO
